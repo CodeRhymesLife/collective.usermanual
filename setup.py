31cd636ce5186f361d0464d2408a1075accd5c6a
Asko Soukka <asko.soukka@iki.fi>
2013-09-29 19:41:29 +0300

    Add test fixture to support test content creating

--- setup.py
+++ setup.py
@@ -1,35 +1,27 @@
 from setuptools import setup, find_packages
-#
-# Note: This file does not really serve any purpose
-# it just keeps some tools happy
-# 
-import os
 
-version = '1.0'
-
-setup(name='collective.usermanual',
-      version=version,
-      description="Plone community maintained developer documentation in Sphinx format",
-      #long_description=open("README.txt").read() + "\n",
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='collective.usermanual',
+    version='1.0.0',
+    description="Plone 4 User Documentation",
+    # Get more strings from
+    # http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='Plone Community',
-      author_email='',
-      url='http://plone.org',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      #namespace_packages=['gomobiletheme'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+    keywords='',
+    author='Plone Community',
+    author_email='',
+    url='https://github.com/collective/collective.usermanual/',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.api',
+        'plone.app.robotframework[speak]',
+        'sphinxcontrib-robotframework[docs]',
+    ]
+)
---
+++ src/collective/usermanual/testing.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from Acquisition import aq_base
+
+from zope.component.hooks import (
+    getSiteManager
+)
+
+from plone.app.robotframework import (
+    AutoLogin,
+    RemoteLibraryLayer
+)
+
+from plone import api
+from plone.uuid.interfaces import IUUID
+from zope.configuration import xmlconfig
+from plone.app.robotframework.remote import RemoteLibrary
+from plone.app.testing import (
+    PLONE_FIXTURE,
+    ploneSite,
+    PloneSandboxLayer)
+from plone.app.testing.layers import FunctionalTesting
+from Products.MailHost.interfaces import IMailHost
+
+from plone.testing import (
+    Layer,
+    z2
+)
+
+
+class MockMailHostLayer(Layer):
+    """Layer for setting up a MockMailHost to store all sent messages as
+    strings into a list at portal.MailHost.messages
+
+    """
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUp(self):
+        # Note: CMFPlone can be imported safely only when a certain
+        # zope.testing-set environment variable is in place.
+        from Products.CMFPlone.tests.utils import MockMailHost
+        with ploneSite() as portal:
+            portal.email_from_address = 'noreply@example.com'
+            portal.email_from_name = 'Plone Site'
+            portal._original_MailHost = portal.MailHost
+            portal.MailHost = mailhost = MockMailHost('MailHost')
+            portal.MailHost.smtp_host = 'localhost'
+            sm = getSiteManager(context=portal)
+            sm.unregisterUtility(provided=IMailHost)
+            sm.registerUtility(mailhost, provided=IMailHost)
+
+    def tearDown(self):
+        with ploneSite() as portal:
+            portal.MailHost = portal._original_MailHost
+            sm = getSiteManager(context=portal)
+            sm.unregisterUtility(provided=IMailHost)
+            sm.registerUtility(aq_base(portal._original_MailHost),
+                               provided=IMailHost)
+
+MOCK_MAILHOST_FIXTURE = MockMailHostLayer()
+
+
+class UserManualLayer(PloneSandboxLayer):
+
+    def setUpZope(self, app, configurationContext):
+        from robot.libraries.BuiltIn import BuiltIn
+
+        for name in BuiltIn().get_variable_value('${META_PACKAGES}', []):
+            if not name in sys.modules:
+                __import__(name)
+            package = sys.modules[name]
+            xmlconfig.file('meta.zcml', package,
+                           context=configurationContext)
+
+        for name in BuiltIn().get_variable_value('${CONFIGURE_PACKAGES}', []):
+            if not name in sys.modules:
+                __import__(name)
+            package = sys.modules[name]
+            xmlconfig.file('configure.zcml', package,
+                           context=configurationContext)
+
+        for name in BuiltIn().get_variable_value('${OVERRIDE_PACKAGES}', []):
+            if not name in sys.modules:
+                __import__(name)
+            package = sys.modules[name]
+            xmlconfig.includeOverrides(
+                configurationContext, 'overrides.zcml', package=package)
+
+        for name in BuiltIn().get_variable_value('${INSTALL_PACKAGES}', []):
+            if not name in sys.modules:
+                __import__(name)
+            package = sys.modules[name]
+            z2.installProduct(app, package)
+
+    def setUpPloneSite(self, portal):
+        from robot.libraries.BuiltIn import BuiltIn
+
+        for name in BuiltIn().get_variable_value('${APPLY_PROFILES}', []):
+            self.applyProfile(portal, name)
+
+USERMANUAL_FIXTURE = UserManualLayer()
+
+
+class CustomRemoteKeywords(RemoteLibrary):
+    """Useful remote keywords, which are run inside a normal ZPublisher-request
+    environment with all the related Plone-magic in place
+
+    """
+
+    def create_content(self, *args, **kwargs):
+        """Create content and return its UID
+        """
+        # XXX: It seems that **kwargs does not yet work with Robot Framework
+        # remote library interface and that's why we need to unpack the
+        # keyword arguments from positional args list.
+        for arg in args:
+            name, value = arg.split('=', 1)
+            kwargs[name] = value
+        assert 'id' in kwargs, u"Keyword arguments must include 'id'."
+        assert 'type' in kwargs, u"Keyword arguments must include 'type'."
+        if 'container' in kwargs:
+            kwargs['container'] = api.content.get(UID=kwargs['container'])
+        else:
+            kwargs['container'] = api.portal.get()
+
+        # Pre-fill Image-types with random content
+        if kwargs.get('type') == 'Image' and not 'image' in kwargs:
+            import random
+            import StringIO
+            from PIL import (
+                Image,
+                ImageDraw
+            )
+            img = Image.new('RGB', (random.randint(320, 640),
+                                    random.randint(320, 640)))
+            draw = ImageDraw.Draw(img)
+            draw.rectangle(((0, 0), img.size), fill=(random.randint(0, 255),
+                                                     random.randint(0, 255),
+                                                     random.randint(0, 255)))
+            del draw
+
+            kwargs['image'] = StringIO.StringIO()
+            img.save(kwargs['image'], 'PNG')
+            kwargs['image'].seek(0)
+
+        return IUUID(api.content.create(**kwargs))
+
+    def get_the_last_sent_email(self):
+        """Return the last sent email from MockMailHost sent messages storage
+        """
+        return self.MailHost.messages[-1] if self.MailHost.messages else u""
+
+USERMANUAL_REMOTE_LIBRARY_FIXTURE = RemoteLibraryLayer(
+    bases=(PLONE_FIXTURE,),
+    libraries=(AutoLogin, CustomRemoteKeywords),
+    name="UserManual:RobotRemote"
+)
+
+USERMANUAL_ROBOT_TESTING = FunctionalTesting(
+    bases=(MOCK_MAILHOST_FIXTURE,
+           USERMANUAL_REMOTE_LIBRARY_FIXTURE,
+           USERMANUAL_FIXTURE,
+           z2.ZSERVER_FIXTURE),
+    name="UserManual:Robot"
+)
