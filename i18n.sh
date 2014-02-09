62c75caced998a4d2785b0794390df034a4d2044
Asko Soukka <asko.soukka@iki.fi>
2014-02-09 10:44:57 +0100

    Move rst sources into root

--- i18n.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bin/pybabel extract source -o src/collective/usermanual/locales/collective.usermanual.pot
-
-bin/i18ndude sync --pot src/collective/usermanual/locales/collective.usermanual.pot src/collective/usermanual/locales/*/LC_MESSAGES/collective.usermanual.po
